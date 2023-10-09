aa5494f96d90d5e8ff43fbd6275c3e6fe149a368
PARKHWANYEONG <welcome0828@naver.com>
2023-10-10 05:37:02 +0900

    feat(Problem1): 연속되는 페이지인지 확인하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        if(!isValidPage(pobi) || !isValidPage(crong)) return -1;
+
+        return 0;
     }
+
+    private static boolean isValidPage(List<Integer> pages) {
+        if(isContinueos(pages)){
+            return false;
+        }
+        
+    }
+    private static boolean isContinueos(List<Integer> pages) {
+        return (pages.get(1) - pages.get(0)) != 1;
+    }
+
 }
