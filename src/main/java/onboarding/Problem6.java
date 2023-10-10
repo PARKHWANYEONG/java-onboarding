0e7117b4780d9deac45e976a98d142e6c0831d6c
PARKHWANYEONG <welcome0828@naver.com>
2023-10-10 19:06:25 +0900

    feat(Problem6): Map에 닉네임을 두 글자로 나누며 리스트에 이메일을 추가하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,34 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Map<String, List<String>> map = nicknameMap(forms);
+
+        return null;
+    }
+
+    private static Map<String, List<String>> nicknameMap(List<List<String>> forms) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (List<String> form : forms) {
+            subStrNickname(map,form);
+        }
+        return map;
+    }
+
+    private static void subStrNickname(Map<String, List<String>> map, List<String> form) {
+        String nickname = form.get(1);
+        String email = form.get(0);
+        for (int i = 0; i < nickname.length()-1; i++) {
+            String substring = nickname.substring(i, i + 2);
+            if (!map.containsKey(substring)) {
+                List<String> emailList = new ArrayList<>();
+                emailList.add(email);
+                map.put(substring, emailList);
+            } else {
+                map.get(substring).add(email);
+            }
+        }
     }
 }
