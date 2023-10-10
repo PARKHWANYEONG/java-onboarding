967b69f7144077f27c5673d44681ea69c5eac801
PARKHWANYEONG <welcome0828@naver.com>
2023-10-10 16:13:52 +0900

    feat(Problem4): 문자를 대소문자 구분하여 요구사항대로 매핑하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.stream.Collectors;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return word.chars()
+                .mapToObj(ch -> String.valueOf((char) charMapping(ch)))
+                .collect(Collectors.joining());
+    }
+
+    private static int charMapping(int ch) {
+        final int lowerCase = 'a'+'z';
+        final int upperCase = 'A'+'Z';
+
+        if (isLowerCase(ch)) {
+            return lowerCase - ch;
+        }
+        if (isUpperCase(ch)) {
+            return upperCase - ch;
+        }
+        return ch;
+    }
+
+    private static boolean isUpperCase(int ch) {
+        return ('A' <= ch && ch <= 'Z');
+    }
+
+    private static boolean isLowerCase(int ch) {
+        return ('a' <= ch && ch <= 'z');
     }
 }
