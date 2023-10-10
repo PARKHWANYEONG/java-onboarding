ba9cadf09a76d915148059459b5802320e0e0eac
PARKHWANYEONG <welcome0828@naver.com>
2023-10-10 15:06:21 +0900

    feat(Problem2): 문자열의 중복 문자를 제거하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        
+        String result = removeDuplication(cryptogram);
+    }
+    private static String removeDuplication(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = cryptogram.toCharArray();
+        char pre = '\u0000';
+        for (char ch : charArray) {
+            if (pre != ch) {
+                stack.push(ch);
+                pre = ch;
+            } else {
+                stack.pop();
+            }
+        }
+        return stack.stream().map(ch -> String.valueOf(ch)).collect(Collectors.joining());
     }
 }
