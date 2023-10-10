ad3ac49dbb1464eabfbf98edfa50bb244a9d7e02
PARKHWANYEONG <welcome0828@naver.com>
2023-10-10 18:18:04 +0900

    feat(Problem5): 화폐 단위 배열을 만들고 순회하여 돈을 단위별로 나누어 리스트에 넣어 반환하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
+    public static final int[] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> result = new ArrayList<>();
+
+        for (int unit : moneyUnit) {
+            exchange(money,unit,result);
+            money = money % unit;
+        }
+        return result;
     }
+    private static void exchange(int money, int unit, List<Integer> result) {
+        result.add(money / unit);
+    }
+
+
 }
