9e9cbb7ed67198d8b32fde0c151a96f31f851148
PARKHWANYEONG <welcome0828@naver.com>
2023-10-10 15:41:21 +0900

    feat(Problem3): 입력받은 숫자에 3,6,9가 들어있는 횟수를 찾는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clapCount = 0;
+
+        for (int i = 1; i <= number; i++) {
+            clapCount += countClap(i);
+        }
+        return clapCount;
+    }
+    private static int countClap(int num) {
+        int count = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit == 3 || digit == 6 || digit == 9) {
+                System.out.println("digit = " + digit);
+                count++;
+            }
+            num /= 10;
+        }
+        return count;
     }
 }
