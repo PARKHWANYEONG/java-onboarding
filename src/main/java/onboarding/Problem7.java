9793c4ffa02ae7534e07bd381717a1dc6ec4a7c8
PARKHWANYEONG <welcome0828@naver.com>
2023-10-11 02:24:29 +0900

    feat(Problem7): friends List에서 user의 친구들을 찾아 리스트로 반환하는 findFriends() 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+        List<String> userFriends = findFriends(user,friends);
+
+
+        return userFriends;
+    }
+    private static List<String> findFriends(String user, List<List<String>> friends) {
+        List<String> userFriends = new ArrayList<>();
+        for (List<String> relation : friends) {
+            if (relation.contains(user)) {
+                addFriendsList(user, userFriends, relation);
+            }
+        }
+        return userFriends;
+    }
+    private static void addFriendsList(String user, List<String> userFriends, List<String> relation) {
+        relation.forEach(v-> {if (!v.equals(user)) {
+            userFriends.add(v);}});
     }
 }
