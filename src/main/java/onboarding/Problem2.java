0bab913b90aa3126220624b6ff67831e3fd081cb
PARKHWANYEONG <welcome0828@naver.com>
2023-10-10 15:12:31 +0900

    feat(Problem2): 문자열 해독이 완료되었는지 확인하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,9 +5,19 @@ import java.util.stream.Collectors;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        
-        String result = removeDuplication(cryptogram);
+
+        while (true) {
+            String result = removeDuplication(cryptogram);
+            if (isComplete(cryptogram, result)) {
+                return cryptogram;
+            }
+            cryptogram = result;
+        }
     }
+    private static boolean isComplete(String cryptogram, String result) {
+        return cryptogram.equals(result);
+    }
+
     private static String removeDuplication(String cryptogram) {
         Stack<Character> stack = new Stack<>();
         char[] charArray = cryptogram.toCharArray();
